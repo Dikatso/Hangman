3d81892e6650f9d7c24ed4f19ee66f0cb4449102
Dikatso Moshweunyane <dikatsogaleboe@gmail.com>
2022-06-17 17:20:52 +0200

    Created lists for each mode

---
+++ Hangman.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Hangman
+{
+    String[] easyMode = {"Doll","Toy","Red","Jug","Van","Yak","Zip"};
+    String[] mediumMode = {"Honey","Mother","Eight","Family","Jelly","Jump","Boat","Dust"};
+    String[] hardMode = {"Drawing","Birthday","Elephant","Animal","Queue","Unable"};
+
+
+}
