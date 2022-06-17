6bc0f15840a6bf9d60d57721197955b31c7eab2a
Dikatso Moshweunyane <dikatsogaleboe@gmail.com>
2022-06-17 20:21:58 +0200

    Error Count switch cases

--- Hangman.java
+++ Hangman.java
@@ -7,23 +7,124 @@ public class Hangman
     String[] hardMode = {"Drawing","Birthday","Elephant","Animal","Queue","Unable"};
 
 
-    static void printBoard()
+    static void printBoard(int numErrors)
     {
-        System.out.println("-------------");
-        System.out.printf("%-11s |\n", "|");
-        System.out.printf("%-11s O\n", "|");
-        System.out.printf("%-10s /|\\ \n", "|");
-        System.out.printf("%-11s |\n", "|");
-        System.out.printf("%-10s / \\\n", "|");
-        System.out.println("|");
-        System.out.println("'------------");
+        System.out.println("------------");
+        System.out.printf("%-11s", "|");
+        switch(numErrors)
+        {
+            case 0:
+                System.out.println("|");
+                System.out.printf("%-11s", "|");
+                System.out.println("");
+                System.out.printf("%-10s", "|");
+                System.out.println("");
+                System.out.printf("%-11s", "|");
+                System.out.println("");
+                System.out.printf("%-10s", "|");
+                System.out.println("");
+                System.out.println("|");
+                System.out.println("'------------\n");
+                break;
 
+            case 1:
+                System.out.println("|");
+                System.out.printf("%-11s", "|");
+                System.out.println("O");
+                System.out.printf("%-10s", "|");
+                System.out.println("");
+                System.out.printf("%-11s", "|");
+                System.out.println("");
+                System.out.printf("%-10s", "|");
+                System.out.println("");
+                System.out.println("|");
+                System.out.println("'------------\n");
+                break;
+            
+            case 2:
+                System.out.println("|");
+                System.out.printf("%-11s", "|");
+                System.out.println("O");
+                System.out.printf("%-11s", "|");
+                System.out.println("|");
+                System.out.printf("%-11s", "|");
+                System.out.println("|");
+                System.out.printf("%-10s", "|");
+                System.out.println("");
+                System.out.println("|");
+                System.out.println("'------------\n");
+                break;
+
+            case 3:
+                System.out.println("|");
+                System.out.printf("%-11s", "|");
+                System.out.println("O");
+                System.out.printf("%-10s", "|");
+                System.out.println("/|");
+                System.out.printf("%-11s", "|");
+                System.out.println("|");
+                System.out.printf("%-10s", "|");
+                System.out.println("");
+                System.out.println("|");
+                System.out.println("'------------\n");
+                break;
+
+            case 4:
+                System.out.println("|");
+                System.out.printf("%-11s", "|");
+                System.out.println("O");
+                System.out.printf("%-10s", "|");
+                System.out.println("/|\\");
+                System.out.printf("%-11s", "|");
+                System.out.println("|");
+                System.out.printf("%-10s", "|");
+                System.out.println("");
+                System.out.println("|");
+                System.out.println("'------------\n");
+                break;
+            
+            case 5:
+                System.out.println("|");
+                System.out.printf("%-11s", "|");
+                System.out.println("O");
+                System.out.printf("%-10s", "|");
+                System.out.println("/|\\");
+                System.out.printf("%-11s", "|");
+                System.out.println("|");
+                System.out.printf("%-10s", "|");
+                System.out.println("/");
+                System.out.println("|");
+                System.out.println("'------------\n");
+                break;
+            
+            case 6:
+                System.out.println("|");
+                System.out.printf("%-11s", "|");
+                System.out.println("O");
+                System.out.printf("%-10s", "|");
+                System.out.println("/|\\");
+                System.out.printf("%-11s", "|");
+                System.out.println("|");
+                System.out.printf("%-10s", "|");
+                System.out.println("/ \\");
+                System.out.println("|");
+                System.out.println("'------------\n");
+                break;
+
+
+        }
 
     }
 
     public static void main(String[] args)
     {
-        printBoard();
+        printBoard(0);
+        printBoard(1);
+        printBoard(2);
+        printBoard(3);
+        printBoard(4);
+        printBoard(5);
+        printBoard(6);
     }
 
 }
